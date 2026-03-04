45a924fa1a7c6b7611ea282b5a87a0e6d8de10ce
RJ Mattson <rjmattson@crimson.ua.edu>
2026-03-04 10:43:50 -0600

    Added a ton of stuff. Still need to do the search function, but should be done with project once thats done. It might take a while though

--- Spring2026-project2-rjmattson/wwwroot/js/site.js
+++ Spring2026-project2-rjmattson/wwwroot/js/site.js
@@ -1,4 +1,42 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿const apiKey = "f8d9983c308509bc2ce2ae985c842ca322a57ede";
+let backgrounds = ["https://images.unsplash.com/photo-1520034475321-cbe63696469a?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", "https://images.unsplash.com/photo-1501862700950-18382cd41497?q=80&w=1038&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D","https://plus.unsplash.com/premium_photo-1721946441813-b39c511e8f92?q=80&w=1618&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"]
+let currentBg = 0;
 
-// Write your JavaScript code.
+$(document).ready(function () {
+    $("button").button();
+
+    $("#query").addClass("ui-widget ui-widget-content ui-corner-all");
+
+    $("#time").dialog({
+        autoOpen: false,
+        modal: true,
+        show: { effect: "blind", duration: 500 }
+    });
+
+    $("#Search").click(function () {
+        theSearch(false);
+    });
+
+    $("#gambleBtn").click(function () {
+        theSearch(true);
+    });
+
+    $("#timeBtn").click(function () {
+        const now = new Date();
+        const formattedTime = now.toTimeString().slice(0, 5);
+        $("#time").text(formattedTime).dialog("open");
+    });
+
+    $("#engineTitle").click(function () {
+        currentBg = (currentBg + 1) % backgrounds.length;
+        $("body").css({
+            "background-image": `url('${backgrounds[currentBg]}')`,
+            "background-size": "cover"
+        });
+
+    });
+});
+
+function theSearch(lucky) {
+
+}
